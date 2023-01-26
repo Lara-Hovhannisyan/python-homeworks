e2d8ac7a74cca0c8e0b5861dde1b26cb7d5bbe5d
Lara-Hovhannisyan <larisa.hovhannisyan7@gmail.com>
2023-01-26 17:39:24 +0400

    create asterisk-pattern.py

---
+++ homework-1/asterisk-pattern.py
@@ -0,0 +1,24 @@
+"""Write program that prints the following image on the screen. Use cycles.
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+"""
+
+n = int(input("Enter a number: "))
+
+
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n - 1:
+            print("*", end=" ")
+        elif j == 0 or j == n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print("")
+
+
+
+
