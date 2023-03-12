0a0789c62bd16ed6c8280df994b1944d69dedf1c
Lara-Hovhannisyan <larisa.hovhannisyan7@gmail.com>
2023-03-12 16:42:47 +0400

    add none version

--- homework-9/filter.py
+++ homework-9/filter.py
@@ -10,3 +10,16 @@ def my_filter(function, iterable):
 x = [1, 2, 3]
 filtered_x = my_filter(lambda y: y > 1, x)
 print(filtered_x)
+
+
+# def my_filter(func, iterable):
+#     res = []
+#     if func is None:
+#         for i in iterable:
+#             if i:
+#                 res.append(i)
+#         yield res
+#     for i in iterable:
+#         if func(i):
+#             res.append(i)
+#     yield res
