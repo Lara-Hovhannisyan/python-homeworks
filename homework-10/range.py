9780be9460c55dc88f0a2e4e6bdc51a89e38ef67
Lara-Hovhannisyan <larisa.hovhannisyan7@gmail.com>
2023-03-12 18:29:45 +0400

    add homework-10

---
+++ homework-10/range.py
@@ -0,0 +1,17 @@
+def my_range(start=0, stop=None, step=1):
+    res = []
+    if stop is None:
+        stop = start
+        start = 0
+    while (step > 0 and start < stop) or (step < 0 and start > stop):
+        res.append(start)
+        start += step
+    return res
+
+ls = [1,2,3,4,5]
+# x = my_range(10, 6, -2)
+for n in my_range(len(ls)):
+  print(n)
+
+
+
